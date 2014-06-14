860cabd707e9aca30d7f21440d8e3eef7d98f5d6
Evan Sosenko <razorx@evansosenko.com>
2014-06-14 01:11:15 -0700

    Change default_spec.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'replace_cookbook::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it "replace_does_something" do
+  it "replace_logs_something" do
     expect(chef_run).to write_log 'replace_this'
   end
 end
