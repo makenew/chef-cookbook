099774623a1dfe6209933214dd4aa9fab174051b
Evan Sosenko <razorx@evansosenko.com>
2014-06-21 17:13:26 -0700

    Added default recipe to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,7 @@ name             'replace_cookbook'
 maintainer       'replace_your_name'
 maintainer_email 'replace_email@example.com'
 license          'Apache 2.0'
-description      'replace_description'
+description      'replace_description.'
 version          '0.0.0'
+
+recipe 'replace_cookbook::default', 'replace_description.'
