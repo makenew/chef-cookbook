96ce982815f6b0a6fad5a0d620f522c06256d12d
Evan Sosenko <razorx@evansosenko.com>
2014-08-04 23:47:39 -0700

    Remove example test.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,7 +4,6 @@ describe 'replace_cookbook::default' do
 
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
-  it "replace_logs_something" do
-    expect(chef_run).to write_log 'replace_this'
+  it "replace_does_something" do
   end
 end
