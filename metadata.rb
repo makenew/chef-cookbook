62337ac046faa9a45fd337456fcd82a942c67110
Evan Sosenko <razorx@evansosenko.com>
2015-05-12 23:41:08 -0700

    Add source_url and issues_url to metadata.

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,8 @@ maintainer       'replace_your_name'
 maintainer_email 'replace_email@example.com'
 license          'Apache 2.0'
 description      'replace_description.'
+source_url       'https://github.com/replace_username/replace_repo'
+issues_url       'https://github.com/replace_username/replace_repo/issues'
 version          '0.0.0'
 
 recipe 'replace_cookbook::default', 'replace_description.'
