45e1d914e18cd54517b93c6dc4a6e0008361b20a
Evan Sosenko <razorx@evansosenko.com>
2015-05-12 20:11:48 -0700

    Use ChefSpec::ServerRunner.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'replace_cookbook::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
 
   it "replace_does_something" do
   end
