006bed647c39f69ad0641e63ab09524b154a8813
Evan Sosenko <razorx@evansosenko.com>
2015-05-12 23:19:45 -0700

    Use single quotes in specs and tests.

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe 'replace_cookbook::default' do
   let(:chef_run) { ChefSpec::ServerRunner.new.converge(described_recipe) }
 
-  it "replace_does_something" do
+  it 'replace_does_something' do
   end
 end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "replace_cookbook::default" do
+describe 'replace_cookbook::default' do
   describe command('ls -al /') do
     its(:stdout) { should match(/bin/) }
   end
