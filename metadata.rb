c83d284f265e28c79a0b027275a03790d9e0689a
Evan Sosenko <razorx@evansosenko.com>
2016-02-18 22:19:44 -0800

    chef-cookbook-v3.2.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache 2.0'
 description      'Chef cookbook skeleton.'
 source_url       'https://github.com/makenew/chef-cookbook'
 issues_url       'https://github.com/makenew/chef-cookbook/issues'
-version          '3.1.0'
+version          '3.2.0'
 
 supports 'ubuntu', '~> 14.04'
 
