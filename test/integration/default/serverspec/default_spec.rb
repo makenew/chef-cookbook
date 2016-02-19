d925e341f19e9a4af3c6b95cc916e2c0b14958a4
Evan Sosenko <razorx@evansosenko.com>
2016-02-18 22:18:53 -0800

    Remove serverspec_helper

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,4 +1,6 @@
-require '../serverspec_helper'
+require 'serverspec'
+set :backend, :exec
+set :path, '/sbin:/usr/sbin:$PATH'
 
 describe 'makenew-chef_cookbook::default' do
   describe command('ls -al /') do
--- test/integration/serverspec_helper.rb
+++
@@ -1,3 +0,0 @@
-require 'serverspec'
-set :backend, :exec
-set :path, '/sbin:/usr/sbin:$PATH'
